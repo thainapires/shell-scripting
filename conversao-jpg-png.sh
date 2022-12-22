e24e7957ce2fb7ebd49ba6765bda1613e1a9e7fc
Thainá Pires <thaina@redeparcerias.com>
2022-12-22 13:40:37 -0300

    add script that converts from jpg to png

---
+++ conversao-jpg-png.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/study/shell-scripting/imagens-livros
+
+for imagem in *.jpg
+do
+    imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+    convert $imagem_sem_extensao.jpg $imagem_sem_extensao.png
+done

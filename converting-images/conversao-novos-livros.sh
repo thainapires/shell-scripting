fa33417d79476b09bde36f74208d8ea34e6218bf
Thainá Pires <thaina@redeparcerias.com>
2022-12-22 17:50:19 -0300

    moving convert images codes to folder

---
+++ converting-images/conversao-novos-livros.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+converte_imagem(){
+    local caminho_imagem=$1
+    local imagem_sem_extensao=$(ls $caminho_imagem | awk -F. '{ print $1 }')
+    convert $imagem_sem_extensao.jpg $imagem_sem_extensao.png
+}
+
+varrer_diretorio(){
+    cd $1
+    for arquivo in *
+    do
+        local caminho_arquivo=$(find ~/study/shell-scripting/converting-images/imagens-novos-livros -name $arquivo)
+        if [ -d $caminho_arquivo ]
+        then
+            #Entrar no diretório e fazer a varredura do conteúdo
+            varrer_diretorio $caminho_arquivo
+        else
+            #Conversao jpg para png
+            converte_imagem $caminho_arquivo
+        fi
+    done
+}
+
+varrer_diretorio ~/study/shell-scripting/imagens-novos-livros
+
+if [ $? -eq 0 ]
+then
+    echo "Conversão realizada com sucesso"
+else
+    echo "Houve um problema na conversão"
+fi
